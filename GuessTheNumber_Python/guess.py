abadf18b48048176196b4c3201417f7381dd1037
Sean Lester Durham <sean.durham1982@gmail.com>
2023-09-19 19:02:11 -0500

    imported random

--- GuessTheNumber_Python/guess.py
+++ GuessTheNumber_Python/guess.py
@@ -1,1 +1,16 @@
-#Test
+import random
+
+
+top_of_range = input("type a number: ")
+
+#checks if the variable top_of_range contains a string that represents a valid integer, and if it does, it converts that string to an integer 
+if top_of_range.isdigit():
+    top_of_range = int(top_of_range)
+
+
+
+
+random_number = random.randint(11)
+print(r)
+
+
