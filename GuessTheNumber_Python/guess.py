535324ff90945599085f00feecbd3727b0eb115d
Sean Lester Durham <sean.durham1982@gmail.com>
2023-09-19 19:10:35 -0500

    added if statements

--- GuessTheNumber_Python/guess.py
+++ GuessTheNumber_Python/guess.py
@@ -6,11 +6,22 @@ top_of_range = input("type a number: ")
 #checks if the variable top_of_range contains a string that represents a valid integer, and if it does, it converts that string to an integer 
 if top_of_range.isdigit():
     top_of_range = int(top_of_range)
+    
+    if top_of_range <= 0:
+        print('Please type a umber larger than 0 next time ')
+        quit()
+        
+else:
+    print('Please type a number next time ')
+    quit()
+
+
+random_number = random.randint(0, top_of_range)
+print(random_number)
 
 
 
 
-random_number = random.randint(11)
-print(r)
+
 
 
