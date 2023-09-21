a2668bfdcdbe103708aa7cb703ecbb4994503ca4
Sean Lester Durham <sean.durham1982@gmail.com>
2023-09-20 20:17:45 -0500

    added the 'guesses' variable for increments

--- GuessTheNumber_Python/guess.py
+++ GuessTheNumber_Python/guess.py
@@ -1,25 +1,41 @@
 import random
 
 
-top_of_range = input("type a number: ")
+top_of_range = input("Type a number: ")
 
-#checks if the variable top_of_range contains a string that represents a valid integer, and if it does, it converts that string to an integer 
+# Check if the input can be converted to an integer
 if top_of_range.isdigit():
     top_of_range = int(top_of_range)
-    
+
     if top_of_range <= 0:
-        print('Please type a umber larger than 0 next time ')
+        print('Please type a number larger than 0 next time.')
         quit()
-        
 else:
-    print('Please type a number next time ')
+    print('Please type a number next time.')
     quit()
 
-
 random_number = random.randint(0, top_of_range)
-print(random_number)
+guesses = 0
 
+while True:
+    guesses += 1
+    user_guess = input("Make a Guess: ")
+    
+    # Check if the input can be converted to an integer
+    if user_guess.isdigit():
+        user_guess = int(user_guess)
+    else:
+        print('Please type a number next time.')
+        continue
 
+    if user_guess == random_number:
+        print("You got it!")
+        break  # End the game if the guess is correct
+    else:
+        print("You got it wrong!")
+    
+print("You got it in", guesses, "guesses")
+   
 
 
 
